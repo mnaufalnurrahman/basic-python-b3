418e2a0e53aab0d03b0f99c37d4e4a0751f5aa76
mnaufalnurrahman <76868740+mnaufalnurrahman@users.noreply.github.com>
2021-01-11 22:56:31 +0700

    SOAL-1.py

---
+++ Tugas-1/SOAL-1.py
@@ -0,0 +1,4 @@
+Nama = str(input("masukan nama:"))
+Umur = int(input("masukan umur:"))
+Tinggi = float(input("masukan tinggi (cm):"))
+print("Nama saya",Nama,"Umur saya",Umur,"tahun dan tinggi saya",Tinggi,"cm")
