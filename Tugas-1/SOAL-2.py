f8d4af05e10703e541b7b005b44140f666f47c41
mnaufalnurrahman <76868740+mnaufalnurrahman@users.noreply.github.com>
2021-01-11 22:57:50 +0700

    SOAL-2.py

---
+++ Tugas-1/SOAL-2.py
@@ -0,0 +1,4 @@
+phi= 22/7
+r=float(input("masukan nilai jari-jari:"))
+Luas_lingkaran= phi*(r**2)
+print("Luas lingkaran dengan jari-jari",r,"cm adalah", "{:.2f}".format(Luas_lingkaran), "cm\u00b2")
